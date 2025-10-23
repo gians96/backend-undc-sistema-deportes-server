48fc587f77ae9da8f1bff1005a4644ec4911a5bb
FernandoYZ <fernandozavala266@gmail.com>
2025-10-23 13:02:41 -0500

    feat: agregar API posiciones

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@
 import { Router } from "express";
 import inscripciones from './inscripciones.routes.js'
 import admin from './admin.routes.js'
+import posicion from "./posiciones.router.js";
 
 const router = Router()
 
 router.use('/api/inscripciones', inscripciones)
 router.use('/api/admin', admin)
+router.use('/api/posiciones', posicion)
 
 export default router
